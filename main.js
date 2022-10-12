079551d4fc37010931320290e7871540f689685f
ArterioRodrigues <arterio369@gmail.com>
2022-10-11 20:48:33 -0400

    animaton update

--- main.js
+++ main.js
@@ -8,17 +8,20 @@ window.onload = async function() {
     let text = document.createElement("p");
 
     let display = ["HI, 👋", "I'm Arterio Rodrigues"];
+    let delay_timers = [1000, 100]
+
 
     for(let i = 0; i < display.length; i++){
         text.classList.add("code");
         text.classList.add("under");
-
+        text.style.backgroundColor = "#44475a";
         for(let j = 0; j < display[i].length; j++){
           
             home.appendChild(text);
-            await delay(1000);
+            await delay(delay_timers[i]);
             text.innerHTML += display[i][j];
         }
+        text.style.backgroundColor = "#282a36"
         text.classList.remove("under");
         text = document.createElement("p");
        
