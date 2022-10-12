5ae174dd09e2bb4dd77b22586d3fa932f4f4bf97
ArterioRodrigues <arterio369@gmail.com>
2022-10-12 14:37:54 -0400

    Update

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ window.onload = async function() {
     let input = document.createElement("input");
 
 
-    let display = ["Hi, 👋", "I'm Arterio Rodrigues", "/n","/n", "I'm a mentor, and motivated computer science major with experience working with C++, Python, Javascript, React and Bash languages and ASM. Knowledgable on full stack development with interest in front-end development (Based in React)." , 
-                    "/n", "do you want my resume:(y/n)"];
-    let delay_timers = [20]
+    let display = ["Hi, 👋", "I'm Arterio Rodrigues", "I'm a mentor, and motivated computer science major with experience working with C++, Python, Javascript,  React and Bash languages and ASM. Knowledgable on full stack development with interest in front-end development (Based in React)." , 
+                    " ", "SKILLS/u"];
+    let delay_timers = [0]
     
     input.classList.add("input");
 
@@ -23,24 +23,35 @@ window.onload = async function() {
         text.style.color = "#ff5555";
 
         for(let j = 0; j < display[i].length; j++){
-          
-            home.appendChild(text);
-            await delay(delay_timers[0]);
             text.innerHTML += display[i][j];
-
+            
             if(text.innerHTML == '/n')
                 text.style.color = "#50fa7b";
 
-            if(text.innerHTML.endsWith('(y/n)')){
-                text.append(input)
-            }
+            if(text.innerHTML.endsWith('(y/n)'))
+                text.append(input);
             
-        }
-        text.style.backgroundColor = "#282a36";
-        
+            if(text.innerHTML.endsWith("/u")){
+                text.style.backgroundColor =  "#ff79c6" ;
+                text.style.display = "inline-block";
+                text.style.color = "#f8f8f2";
+                text.style.padding = "5px 20px";
+                text.style.marginTop = "20px";
+            }
 
-        if(text.innerHTML != '/n')
+            await delay(delay_timers[0]);
+            home.appendChild(text);
+           
+        }
+
+        if(!text.innerHTML.endsWith("/u"))
+            text.style.backgroundColor = "#282a36";
+        
+    
+
+        if(text.innerHTML != '/n' && !text.innerHTML.endsWith("/u"))
             text.style.color = "#8be9fd";
+        
         text.classList.remove("under");
         text = document.createElement("p");
     }
